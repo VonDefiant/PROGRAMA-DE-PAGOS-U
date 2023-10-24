daae7dd1868998584b0f12791b0be3db11c3be6d
alejoco626 <alejandrocarrillorellana@gmail.com>
2023-10-24 16:54:46 -0600

    Agregue las clases estudiante, facultades, curso, profesor y usuario al igual que un nuevo form para estudiante (este ultimo aun esta en blanco)-Alejandro Carrillo

---
+++ programa-mamalon-de-pagos/BACKEND/Facultades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_mamalon_de_pagos.BACKEND
+{
+    public class Facultades
+    {
+        //Propiedades
+        public string? IdFacultad { get; set; }
+        public string? Nombre { get; set; }
+        public string? Institucion { get; set; }
+
+        //Constructor
+        public Facultades(string idFacultad, string nombre, string institucion)
+        {
+            IdFacultad = idFacultad;
+            Nombre = nombre;
+            Institucion = institucion;
+        }
+    }
+}
